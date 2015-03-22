b6369fd5f88a39ade7d511e197778796d420eb86
ddilley926 <dustin.dilley@hotmail.com>
2015-03-21 19:42:24 -0700

    Finished First Event

--- src/com/dustinlearning/events/BasicEvent.java
+++ src/com/dustinlearning/events/BasicEvent.java
@@ -1,5 +1,28 @@
 package com.dustinlearning.events;
 
-public class BasicEvent {
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BasicEvent extends JavaPlugin implements Listener{
+
+public void onEnable(){
+
+PluginManager pm = getServer().getPluginManager();
+pm.registerEvents(this, this);
+
+
+}
+@EventHandler 
+public void onJoin(PlayerJoinEvent e){
+	Player p = e.getPlayer();
+		p.sendMessage(ChatColor.GOLD + "Thanks for joining" + ChatColor.BLUE + ChatColor.BOLD + "Cosmic Network" + p.getName());
+
+
+}
 
 }
